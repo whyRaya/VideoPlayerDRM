d15a0db09581346744bc0524702ce1d81ad69fcc
Wahyu Anggara Raya <wahyu.anggara@hiaria.id>
2023-07-02 13:52:37 +0700

    feat: first new init

---
+++ app/src/main/java/com/whyraya/videoplayerdrm/VideoViewModel.kt
@@ -0,0 +1,9 @@
+package com.whyraya.videoplayerdrm
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoViewModel @Inject constructor() : ViewModel() {
+}
